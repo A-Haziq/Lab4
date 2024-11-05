9a835d84530d28494bcb51acc368b3c7596e987f
Haziq <amirulhaziqba08.23@ichat.sp.edu.sg>
2024-11-05 17:02:04 +0800

    Lab4

--- src/PiLedTest.py
+++ src/PiLedTest.py
@@ -1,8 +1,9 @@
-import socket
 import time
 from time import sleep
 
 from hal import hal_led as led
+from hal import hal_input_switch as switch_init
+from hal import hal_input_switch as read_slide_switch
 
 def blink_led(delay):
     # Led Blink
@@ -14,4 +15,19 @@ def blink_led(delay):
     led.set_output(0, 0)
     time.sleep(delay)
 
-def main:
+def main():
+
+    led.init()
+    switch_init()
+
+    delay = 0.1
+
+    while True:
+        if read_slide_switch() == 0:
+            blink_led(delay)
+        else:
+            led.set_output(0, 0)
+            time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
